9b9851dcd15e0039e498734322ea710d418c1f63
Alexander Amini <xan.amini@gmail.com>
2019-09-07 13:11:34 -0400

    default xbox joystick to launchfile

--- launch/h_log_with_joystick.py
+++ launch/h_log_with_joystick.py
@@ -1,11 +1,14 @@
 <?xml version="1.0"?>
 
 <launch>
-    <arg name="out_path" default="$(env BEBOP_RECORD_PATH)"/>
+    <arg name="out_path" default="$(env HOME)/drone_data"/>
     <arg name="id" default=""/>
 
     <!-- <include file="$(find bebop_driver)/launch/bebop_node.launch"/> -->
-    <include file="$(find bebop_tools)/launch/joy_teleop.launch"/>
+    <include file="$(find bebop_tools)/launch/joy_teleop.launch"
+        <arg name="joy_config" value="xbox360"/>
+    />
+
     <node pkg="deepdrone" name="move_camera_down" type="move_camera_down.py" output="screen" />
 
     <node pkg="rosbag"
