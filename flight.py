84d5e96352750631b1a320db5f450579dcc17c63
Charlie Vorbach <cvorbach@mit.edu>
2020-09-08 18:01:50 -0400

    Refactored, solved the visibility problem

--- flight.py
+++ flight.py
@@ -10,7 +10,7 @@ import pprint
 import heapq
 import pickle
 
-from scipy.interpolate import UnivariateSpline
+from scipy.spatial.transform import Rotation as R
 
 # Images to collect
 imagesRequests = [
@@ -63,18 +63,24 @@ client.confirmConnection()
 client.enableApiControl(True) 
 
 # Constants
-ENDPOINT_TOLERANCE = 1.0
-ENDPOINT_RADIUS = 15
-PLOT_PERIOD = 7.0
-PLOT_DELAY = 2.0
-CONTROL_PERIOD = 0.5
-SPEED = 0.5 # m/s
-YAW_TIMEOUT = 0.1
-# BOUNDING_RADIUS = 2.5
-VOXEL_SIZE = 1.0
-LOOK_AHEAD_DIST = 1.0
-ENABLE_PLOTTING = False
-ENABLE_RECORDING = True
+ENDPOINT_TOLERANCE    = 2.5       # m
+ENDPOINT_RADIUS       = 15        # m
+PLOT_PERIOD           = 10.0      # s
+PLOT_DELAY            = 3.0       # s
+CONTROL_PERIOD        = 0.75      # s
+SPEED                 = 0.5       # m/s
+YAW_TIMEOUT           = 0.1       # s
+VOXEL_SIZE            = 1.0       # m
+LOOK_AHEAD_DIST       = 1.0       # m
+MAX_ENDPOINT_ATTEMPTS = 50
+N_RUNS                = 1000
+ENABLE_PLOTTING       = False
+ENABLE_RECORDING      = True
+
+CAMERA_FOV = np.pi / 6
+RADIANS_2_DEGREES = 180 / np.pi
+CAMERA_VERTICAL_OFFSET = np.array([0, 0, -0.5])
+#TODO(cvorbach) CAMERA_HORIZONTAL_OFFSET
 
 # Utilities
 class SparseVoxelOccupancyMap:
@@ -127,7 +133,7 @@ class SparseVoxelOccupancyMap:
         return voxel in self.occupiedVoxels
     
     def point2Voxel(self, point):
-        return tuple(self.voxelSize * int(v / self.voxelSize + 0.5) for v in point)
+        return tuple(self.voxelSize * int(round(v / self.voxelSize)) for v in point)
 
     def getNeighbors(self, voxel):
         neighbors = []
@@ -146,6 +152,39 @@ class SparseVoxelOccupancyMap:
     def plotOccupancies(self):
         occupiedPoints = [Vector3r(float(v[0]), float(v[1]), float(v[2])) for v in self.occupiedVoxels]
         client.simPlotPoints(occupiedPoints, color_rgba = [0.0, 0.0, 1.0, 1.0], duration=PLOT_PERIOD-PLOT_DELAY) 
+
+
+def isVisible(point, position, orientation):
+    # Edge case point == position
+    if np.linalg.norm(point - position) < 0.05:
+        return True
+
+    # Check if endpoint is in frustrum
+    xUnit = np.array([1, 0, 0])
+    cameraDirection = R.from_quat(orientation).apply(xUnit)
+
+    endpointDirection = point - position
+    endpointDirection /= np.linalg.norm(endpointDirection)
+
+    # TODO(cvorbach) check square, not circle
+    angle = np.arccos(np.dot(cameraDirection, endpointDirection)) 
+
+    if abs(angle) > CAMERA_FOV:
+        return False
+
+    # TODO(cvorbach) Check for occlusions with ray-tracing
+
+    return True
+
+
+def orientationAt(endpoint, position):
+    # Get the drone orientation that faces towards the endpoint at position
+    displacement = endpoint - position
+    endpointYaw = np.arctan2(displacement[1], displacement[0])
+    orientation = R.from_euler('xyz', [0, 0, endpointYaw]).as_quat()
+
+    return orientation
+
 
 def h(voxel, map, endpoint):
     return np.linalg.norm(endpoint - np.array(voxel))
@@ -180,6 +219,11 @@ def findPath(startpoint, endpoint, map):
             return list(reversed(path))
 
         for neighbor in map.getNeighbors(current):
+            # skip neighbors from which the endpoint isn't visible
+            neighborOrientation = orientationAt(endpoint, neighbor)
+            if not isVisible(np.array(end), np.array(neighbor), neighborOrientation):
+                continue
+
             tentativeGScore = gScore.get(current, float("inf")) + d(current, neighbor, map)
 
             if tentativeGScore < gScore.get(neighbor, float('inf')):
@@ -197,41 +241,103 @@ def findPath(startpoint, endpoint, map):
         
     raise ValueError("Couldn't find a path")
 
-# Takeoff
-client.armDisarm(True)
-client.takeoffAsync().join()
-print("Taken off")
 
-try:
-    with open("occupancy_map.p", 'rb') as f:
-        map = pickle.load(f)
-except:
-    map = SparseVoxelOccupancyMap(VOXEL_SIZE)
+def getTime():
+    return 1e-9 * client.getMultirotorState().timestamp
 
-for i in range(100):
-    controlThread = None
 
-    endpoint = map.point2Voxel(ENDPOINT_RADIUS * np.array([random.random(), random.random(), -random.random()]))
-    client.simPlotPoints([Vector3r(endpoint[0], endpoint[1], endpoint[2])], is_persistent = True) 
+def getPose():
+    pose        = client.simGetVehiclePose()
+    position    = pose.position.to_numpy_array() - CAMERA_VERTICAL_OFFSET
+    orientation = pose.orientation.to_numpy_array()
+    return position, orientation
 
-    pose = client.simGetVehiclePose()
-    position = pose.position.to_numpy_array()
+
+def isValidEndpoint(endpoint, map):
+    if map.isOccupied(endpoint):
+        return False
+
+    position, orientation = getPose()
+    if not isVisible(endpoint, position, orientation):
+        return False
+
+    # TODO(cvorbach) Check there is a valid path
+
+    return True
+
+
+def generateEndpoint(map):
+    isValid = False
+    attempts = 0
+    while not isValid:
+        # TODO(cvorbach) generate points already in the camera frustrum
+        endpoint = map.point2Voxel(ENDPOINT_RADIUS * np.array([random.random(), random.random(), -random.random()]))
+        isValid = isValidEndpoint(endpoint, map)
+
+        attempts += 1
+        if attempts > MAX_ENDPOINT_ATTEMPTS:
+            return None
+
+    return np.array(endpoint)
+
+
+def turnTowardEndpoint(endpoint, timeout=0.01):
+    position, _ = getPose()
     displacement = endpoint - position
 
-    endpointYaw = np.arctan2(displacement[1], displacement[0]) * 180 / 3.14
-    client.rotateToYawAsync(endpointYaw).join()
-    print("Pointed to endpoint")
+    endpointYaw = np.arctan2(displacement[1], displacement[0]) * RADIANS_2_DEGREES
+    client.rotateToYawAsync(endpointYaw, timeout_sec = timeout).join()
+    print("Turned toward endpoint")
 
-    lastPlotTime = 0
 
-    if ENABLE_RECORDING:
-        client.startRecording()
-    while np.linalg.norm(endpoint - position) > ENDPOINT_TOLERANCE:
-        time = 1e-9 * client.getMultirotorState().timestamp
+def tryPlotting(lastPlotTime, trajectory, map):
+    if getTime() < PLOT_PERIOD + lastPlotTime:
+        return lastPlotTime
 
-        pose = client.simGetVehiclePose()
-        position = pose.position.to_numpy_array()
-        displacement = endpoint - position
+    plottableTrajectory = [Vector3r(float(trajectory[i][0]), float(trajectory[i][1]), float(trajectory[i][2])) for i in range(len(trajectory))]
+    client.simPlotPoints(plottableTrajectory, color_rgba = [0.0, 1.0, 0.0, 1.0], duration=PLOT_PERIOD-PLOT_DELAY) 
+
+    map.plotOccupancies()
+
+    print("Replotted :)")
+    return getTime()
+
+
+def updateOccupancies(map):
+    lidarData = client.getLidarData()
+    lidarPoints = np.array(lidarData.point_cloud, dtype=np.dtype('f4'))
+    if len(lidarPoints) >=3:
+        lidarPoints = np.reshape(lidarPoints, (lidarPoints.shape[0] // 3, 3))
+
+        for p in lidarPoints:
+            map.addPoint(p)
+
+    print("Lidar data added")
+
+
+def pursuitVelocity(trajectory):
+    # Find lookahead point
+    position, _ = getPose()
+    lookAheadPoint = trajectory[-1]
+    for i in range(1, len(trajectory)):
+        lookAheadPoint = trajectory[i]
+        if np.linalg.norm(lookAheadPoint - position) > LOOK_AHEAD_DIST: # TODO(cvorbach) interpolate properly
+            break
+
+    # Compute velocity to pursue lookahead point
+    pursuitVector = lookAheadPoint - position
+    pursuitVector = SPEED / np.linalg.norm(pursuitVector) * pursuitVector
+
+    return pursuitVector
+
+
+def moveToEndpoint(endpoint):
+    controlThread   = None
+    reachedEndpoint = False
+    lastPlotTime    = 0
+
+    while not reachedEndpoint:
+        position, _ = getPose()
 
         if map.isOccupied(endpoint):
             print("Endpoint is occupied")
@@ -240,60 +346,70 @@ for i in range(100):
         if map.isOccupied(position):
             print("Drone in occupied position")
 
-        endpointYaw = np.arctan2(displacement[0], displacement[1])
-
-        lidarData = client.getLidarData()
-        lidarPoints = np.array(lidarData.point_cloud, dtype=np.dtype('f4'))
-        if len(lidarPoints) >=3:
-            lidarPoints = np.reshape(lidarPoints, (lidarPoints.shape[0] // 3, 3))
-
-            for p in lidarPoints:
-                map.addPoint(p)
-
-        print("Lidar data added")
-
+        updateOccupancies(map)
 
         trajectory = findPath(position, endpoint, map)
+        velocity   = pursuitVelocity(trajectory)
 
-        # xSpline = UnivariateSpline(np.linspace(0, 1, num=len(trajectory)), [p[0] for p in trajectory])
-        # ySpline = UnivariateSpline(np.linspace(0, 1, num=len(trajectory)), [p[1] for p in trajectory])
-        # zSpline = UnivariateSpline(np.linspace(0, 1, num=len(trajectory)), [p[2] for p in trajectory])
-
-        # nextStep = np.array([xSpline(0.1), ySpline(0.1), zSpline(0.1)])
-
-        trajectoryLine = [Vector3r(float(trajectory[i][0]), float(trajectory[i][1]), float(trajectory[i][2])) for i in range(len(trajectory))]
-
-        if ENABLE_PLOTTING and time >= PLOT_PERIOD + lastPlotTime:
-            lastPlotTime = time
-            client.simPlotPoints(trajectoryLine, color_rgba = [0.0, 1.0, 0.0, 1.0], duration=PLOT_PERIOD-PLOT_DELAY) 
-            map.plotOccupancies()
-            print("Replotted :)")
-
-        for i in range(1, len(trajectory)):
-            lookAheadPoint = trajectory[i]
-            if np.linalg.norm(lookAheadPoint - position) > LOOK_AHEAD_DIST: # TODO(cvorbach) interpolate properly
-                break
-        nextStep = lookAheadPoint - position
-        nextStep = SPEED / np.linalg.norm(nextStep) * nextStep
+        if ENABLE_PLOTTING:
+            lastPlotTime = tryPlotting(lastPlotTime, trajectory, map)
 
         if controlThread is not None:
             controlThread.join()
 
-        endpointYaw = np.arctan2(displacement[1], displacement[0]) * 180 / 3.14
-        client.rotateToYawAsync(endpointYaw, timeout_sec = 0.1).join()
-        print("Turning")
+        turnTowardEndpoint(endpoint)
 
-        controlThread = client.moveByVelocityAsync(float(nextStep[0]), float(nextStep[1]), float(nextStep[2]), CONTROL_PERIOD)
+        controlThread = client.moveByVelocityAsync(float(velocity[0]), float(velocity[1]), float(velocity[2]), CONTROL_PERIOD)
         print("Moving")
+        
+        reachedEndpoint = np.linalg.norm(endpoint - position) <= ENDPOINT_TOLERANCE
+
+    # Finish moving
+    if controlThread is not None:
+        controlThread.join()
+
+# -----------------------------
+# MAIN
+# -----------------------------
+
+# Takeoff
+client.armDisarm(True)
+client.takeoffAsync().join()
+print("Taken off")
+
+# Try to load saved occupancy map
+try:
+    with open("occupancy_map.p", 'rb') as f:
+        map = pickle.load(f)
+except:
+    map = SparseVoxelOccupancyMap(VOXEL_SIZE)
+ 
+# Collect data runs
+for i in range(N_RUNS):
+    # Random rotation
+    client.rotateToYawAsync(random.random() * 2.0 * np.pi * RADIANS_2_DEGREES).join()
+
+    # Set up
+    endpoint = generateEndpoint(map)
+    if endpoint is None:
+        continue
+    client.simPlotPoints([Vector3r(endpoint[0], endpoint[1], endpoint[2])], size=100, is_persistent = True) 
+
+    turnTowardEndpoint(endpoint, timeout=10.0)
+
+    if ENABLE_RECORDING:
+        client.startRecording()
+
+    # Control loop
+    moveToEndpoint(endpoint)
+
+    # Clean up
+    if ENABLE_RECORDING:
+        client.stopRecording()
 
     with open('occupancy_map.p', 'wb') as f:
         pickle.dump(map, f)
 
-    if ENABLE_RECORDING:
-        client.stopRecording()
-
-    if controlThread is not None:
-        controlThread.join()
     client.simFlushPersistentMarkers()
 
 #     time.sleep(0.1)
