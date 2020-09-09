e44aad9d601aa6c757e105e9229a2af4532be9f0
Charlie Vorbach <cvorbach@mit.edu>
2020-09-09 11:45:07 -0400

    Training works, some work remains to reduce memory consumption

--- training.py
+++ training.py
@@ -10,118 +10,97 @@ from tensorflow import keras
 import kerasncp as kncp
 from datetime import datetime
 
-TRAINING_DATA_DIRECTORY  = os.getcwd() + '/training-data'
-TRAINING_SEQUENCE_LENGTH = 100
+TRAINING_DATA_DIRECTORY    = os.getcwd() + '/data'
+MODEL_CHECKPOINT_DIRECTORY = os.getcwd() + '/model-checkpoints'
+TRAINING_MAX_SAMPLES       = 32
+TRAINING_SEQUENCE_LENGTH   = 32
+IMAGE_WIDTH                = 256
+IMAGE_HEIGHT               = 256
+CHANNELS                   = 3
 
-trainingSamples = len(os.listdir(TRAINING_DATA_DIRECTORY))
-print(trainingSamples)
+
+# Load the data from file
+# TODO(cvorbach) do this data processing in other script
+# TODO(cvorbach) stream data from file with generator
+# TODO(cvorbach) validation set
+trainingSamples = min(len(os.listdir(TRAINING_DATA_DIRECTORY)), TRAINING_MAX_SAMPLES)
+print(f"{trainingSamples} Training Samples")
+
+xTrain = np.zeros((trainingSamples, TRAINING_SEQUENCE_LENGTH, IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS))
+yTrain = np.zeros((trainingSamples, TRAINING_SEQUENCE_LENGTH, 3))
+
+for i, runDirectory in enumerate(os.listdir(TRAINING_DATA_DIRECTORY)):
+    # stop loading data if we have enough samples
+    if i >= TRAINING_MAX_SAMPLES:
+        break
+
+    imageDirectory = TRAINING_DATA_DIRECTORY + '/' + runDirectory + '/images'
+    odometryFile   = TRAINING_DATA_DIRECTORY + '/' + runDirectory + '/airsim_rec.npy'
+
+    for j, imageFile in enumerate(os.listdir(imageDirectory)):
+        if j >= TRAINING_SEQUENCE_LENGTH:
+            break
+        xTrain[i][j] = np.load(imageDirectory + '/' + imageFile)
+
+    odometry = np.load(odometryFile)
+    for j, record in enumerate(odometry[1:]): # image[i] predicts position[i+1]
+        if j >= TRAINING_SEQUENCE_LENGTH:
+            break
+        yTrain[i][j-1] = np.array([record['x'], record['y'], record['z']])
 
 
+# Setup the network
+wiring = kncp.wirings.NCP(
+    inter_neurons=12,   # Number of inter neurons
+    command_neurons=8,  # Number of command neurons
+    motor_neurons=3,    # Number of motor neurons
+    sensory_fanout=4,   # How many outgoing synapses has each sensory neuron
+    inter_fanout=4,     # How many outgoing synapses has each inter neuron
+    recurrent_command_synapses=4,   # Now many recurrent synapses are in the
+                                    # command neuron layer
+    motor_fanin=6,      # How many incomming syanpses has each motor neuron
+)
 
-# # Load the data
-# dataRuns = []
-# dataDirectory = os.getcwd() + '\\training-data'
-# imageOdometryType = [('timestamp', np.uint64), ('x', np.float32), ('y', np.float32), ('z', np.float32), ('qw', np.float32), ('qx', np.float32), ('qy', np.float32), ('qz', np.float32), ('imagefile', 'U32')]
-# 
-# # for runDirectory in os.listdir(dataDirectory):
-# #     imageDirectory = dataDirectory + '\\' + runDirectory + '\\images'
-# # 
-# #     if len(os.listdir(imageDirectory)) <= 1:
-# #         print(imageDirectory)
-# 
-# 
-# for n, runDirectory in enumerate(os.listdir(dataDirectory)):
-#     if n > 10:
-#         break
-#     imageDirectory = dataDirectory + '\\' + runDirectory + '\\images'
-#     odometryFile   = dataDirectory + '\\' + runDirectory + '\\airsim_rec.txt'
-# 
-#     odometry = np.array(np.genfromtxt(fname=odometryFile, dtype=imageOdometryType, skip_header=1))
-#     validImages = np.full((len(odometry),), fill_value=False)
-# 
-#     imageMap = dict()
-#     for i, record in enumerate(odometry):
-#         imageFile = str(record["imagefile"])
-#         try:
-#             imageMap[imageFile] = np.array(PIL.Image.open(imageDirectory + '\\' + imageFile).convert('RGB')) 
-#             validImages[i]     = True
-#         except PIL.UnidentifiedImageError:
-#             pass
-# 
-#     odometry = odometry[validImages]
-# 
-#     # print(imageMap[imageFile].shape)
-# 
-#     images = np.zeros((len(odometry), *(imageMap[imageFile].shape)))
-# 
-#     for i, record in enumerate(odometry):
-#         images[i] = imageMap[record['imagefile']]
-# 
-#     dataRuns.append((odometry, images))
-# 
-# # Setup the network
-# wiring = kncp.wirings.NCP(
-#     inter_neurons=12,   # Number of inter neurons
-#     command_neurons=8,  # Number of command neurons
-#     motor_neurons=3,    # Number of motor neurons
-#     sensory_fanout=4,   # How many outgoing synapses has each sensory neuron
-#     inter_fanout=4,     # How many outgoing synapses has each inter neuron
-#     recurrent_command_synapses=4,   # Now many recurrent synapses are in the
-#                                     # command neuron layer
-#     motor_fanin=6,      # How many incomming syanpses has each motor neuron
-# )
-# rnnCell = kncp.LTCCell(wiring)
-# 
-# imageShape = dataRuns[0][1].shape # (Sample Count, 144, 256, 3)
-# 
-# model = keras.models.Sequential()
-# model.add(keras.Input(shape=(None, *imageShape[1:])))
-# model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu')))
-# model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu')))
-# model.add(keras.layers.TimeDistributed(keras.layers.MaxPool2D(pool_size=(2,2), strides=2)))
-# model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu')))
-# model.add(keras.layers.TimeDistributed(keras.layers.MaxPool2D(pool_size=(2,2))))
-# model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu')))
-# model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
-# model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=24, activation='relu')))
-# model.add(keras.layers.RNN(rnnCell, return_sequences=True))
-# 
-# model.compile(
-#     optimizer=keras.optimizers.Adam(0.01), loss="mean_squared_error",
-# )
-# 
-# # print("Output: ", model.output)
-# # print("Input: ", model.input)
-# 
-# # model.summary(line_length=100)
-# 
-# # Plot NCP wiring
-# # sns.set_style("white")
-# # plt.figure(figsize=(12, 12))
-# # legend_handles = rnnCell.draw_graph(layout='spiral',neuron_colors={"command": "tab:cyan"})
-# # plt.legend(handles=legend_handles, loc="upper center", bbox_to_anchor=(1, 1))
-# # sns.despine(left=True, bottom=True)
-# # plt.tight_layout()
-# # plt.show()
-# 
-# # Train
-# 
-# for i in range(10):
-#     for run in dataRuns:
-#         xTrain = np.array([run[1][:-1]])
-#         yTrain = np.array([[[odometry['x'], odometry['y'], odometry['z']] for odometry in run[0][1:]]])
-# 
-#         # print("x: ", xTrain.shape)
-#         # print("y: ", yTrain.shape)
-# 
-#         model.fit(
-#             x=xTrain, y=yTrain, epochs=5
-#         )
-# 
-#         model.save('model-checkpoints/' + datetime.now() + '-model.p')
-# 
-# model.evaluate(xTrain, yTrain)
-# 
+rnnCell = kncp.LTCCell(wiring)
+
+model = keras.models.Sequential()
+model.add(keras.Input(shape=xTrain.shape[1:]))
+model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=36, kernel_size=(5,5), strides=(2,2), activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=48, kernel_size=(3,3), strides=(2,2), activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
+model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.5)))
+model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=1000, activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.5)))
+model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=100,  activation='relu')))
+model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.3)))
+model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=24,   activation='relu')))
+model.add(keras.layers.RNN(rnnCell, return_sequences=True))
+
+model.compile(
+    optimizer=keras.optimizers.Adam(0.01), loss="mean_squared_error",
+)
+
+model.summary(line_length=80)
+
+# Train
+checkpointCallback = keras.callbacks.ModelCheckpoint(
+    filepath=MODEL_CHECKPOINT_DIRECTORY,
+    save_weights_only=True,
+    save_freq=5
+)
+
+model.fit(
+    xTrain,
+    yTrain,
+    batch_size      = 8,
+    epochs          = 200,
+    # validation_data = (xValid, yValid),
+    callbacks       = [checkpointCallback]
+)
+
 # # Offload 
-# 
+
 # # Visualize?
