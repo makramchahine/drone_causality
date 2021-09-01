c5dc1f7122b8a47e4b43eb5c5bfc1e5a94ba3ffb
raminmh <rhasani@mit.edu>
2021-09-01 15:02:00 -0400

    first

--- keras_models.py
+++ keras_models.py
@@ -2,22 +2,23 @@ from tensorflow import keras
 from kerasncp.tf import LTCCell
 import kerasncp as kncp
 
+LSTM_DROPOUT = 0.1
+LSTM_RECURRENT_DROPOUT = 0.1
+DROPOUT = 0.1
 
-def generate_lstm_model(rnn_sizes, seq_len, image_shape, do_normalization, do_augmentation, data, augmentation_params=None, rnn_stateful=False, batch_size=None):
+def generate_lstm_model(rnn_sizes, seq_len, image_shape, do_normalization, do_augmentation, data, augmentation_params=None, rnn_stateful=True, batch_size=None):
     lstm_model = generate_network_trunk(seq_len, image_shape, do_normalization, do_augmentation, data, augmentation_params, rnn_stateful=rnn_stateful, batch_size=batch_size)
-
-    print(lstm_model.summary())
 
     #print(lstm_model.layers[-1].output_shape)
     #print(batch_size, seq_len, lstm_model.layers[-1].output_shape[-1])
     for (ix, s) in enumerate(rnn_sizes):
-        lstm_model.add(keras.layers.LSTM(s, batch_input_shape=(batch_size, seq_len, lstm_model.layers[-1].output_shape[-1]), return_sequences=True, stateful=rnn_stateful, dropout=0.2, recurrent_dropout=0.1))
-        if ix < len(rnn_sizes) - 1:
-            lstm_model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.1)))
+        lstm_model.add(keras.layers.LSTM(s, batch_input_shape=(batch_size, seq_len, lstm_model.layers[-1].output_shape[-1]), return_sequences=True, stateful=rnn_stateful, dropout=LSTM_DROPOUT, recurrent_dropout=LSTM_RECURRENT_DROPOUT))
+        #if ix < len(rnn_sizes) - 1:
+        #    lstm_model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.1)))
     
     #lstm_model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.05)))
     lstm_model.add(keras.layers.Dense(units=4, activation='linear'))
-
+    print(lstm_model.summary())
     return lstm_model
 
 
@@ -47,13 +48,12 @@ def generate_ncp_model(seq_len, image_shape, do_normalization, do_augmentation, 
 
 def generate_convolutional_layers(model):
 
-
     model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation='relu')))
     model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=36, kernel_size=(5,5), strides=(2,2), activation='relu')))
     model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=48, kernel_size=(5,5), strides=(2,2), activation='relu')))
     model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu')))
     model.add(keras.layers.TimeDistributed(keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu')))
-
+    
     return model
 
 
@@ -83,23 +83,75 @@ def generate_augmentation_layers(model, augmentation_params):
 
 def generate_network_trunk(seq_len, image_shape, do_normalization, do_augmentation, data=None, augmentation_params=None, rnn_stateful=False, batch_size=None):
 
+    # model.add(keras.layers.InputLayer(input_shape=(seq_len, *image_shape), batch_size=batch_size))
+    # model.add(keras.layers.InputLayer(batch_input_shape=(batch_size, seq_len, *image_shape)))
+    # inputs = keras.Input(batch_input_shape=(batch_size,seq_len,*image_shape))
+    # rescaling_layer = keras.layers.experimental.preprocessing.Rescaling(1./255)
+    # normalization_layer = keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072], variance=[0.19504249, 0.18745404, 0.20891384])
+    # inputs = rescaling_layer(inputs)
+    # inputs = normalization_layer(inputs)
+    # #inputs =keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072], variance=[0.19504249, 0.18745404, 0.20891384])(inputs)
+    # #model.add(keras.layers.InputLayer(batch_input_shape=(batch_size, seq_len, 144, 256, 3)))
+    # #model.add(keras.Input(batch_shape=(6, seq_len, *image_shape)))
+
+    # #if do_normalization:
+    # #    model = generate_normalization_layers(model)
+    # #
+    # # if do_augmentation:
+    # #     model = generate_augmentation_layers(model, augmentation_params)
+
+    # model_vgg = keras.applications.VGG16(include_top=False, weights='imagenet',input_shape=image_shape)
+    # model_vgg.trainable = False
+    # intermediate_model = keras.Model(inputs=model_vgg.input, outputs=model_vgg.get_layer('block5_pool').output)
+    # time_dist_layers = keras.layers.TimellayerDistributed(intermediate_model)(inputs)
+    # my_time_model = keras.Model(inputs=inputs, outputs=time_dist_layers)
+
+
+
+    # model = keras.models.Sequential()
+
+    # model.add(my_time_model)
+
+    # #model = generate_convolutional_layers(model)
+    # print(model.summary())
+    # model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
+    # model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=1024,   activation='linear')))
+    # model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=DROPOUT)))
+    
+    #x = model.layers[0].output
+    #x = keras.layers.experimental.preprocessing.Rescaling(1./255)(x)
+     
+    model_vgg = keras.applications.VGG16(include_top=False, weights='imagenet',input_shape=image_shape)
+    layers = [l for l in model_vgg.layers]
+
+    inputs = keras.Input(batch_input_shape=(batch_size,seq_len,*image_shape))
+    rescaling_layer = keras.layers.experimental.preprocessing.Rescaling(1./255)
+    #normalization_layer = keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072], variance=[0.19504249, 0.18745404, 0.20891384])
+    normalization_layer = keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072], variance=[.057,.05,.061])
+    layers[0] = inputs
+    #x = layers[0]
+    x = rescaling_layer(layers[0])
+    x = keras.layers.TimeDistributed(normalization_layer)(x)
+
+    for i in range(len(layers)):
+        if i == 0:
+            continue
+        else:
+            layers[i].trainable = False
+            layers[i] = keras.layers.TimeDistributed(layers[i])
+            x = layers[i](x)
+    
+    my_time_model = keras.Model(inputs=inputs, outputs=x)
+
     model = keras.models.Sequential()
-    #model.add(keras.layers.InputLayer(input_shape=(seq_len, *image_shape), batch_size=batch_size))
-    model.add(keras.layers.InputLayer(batch_input_shape=(batch_size, seq_len, *image_shape)))
-    #model.add(keras.layers.InputLayer(batch_input_shape=(batch_size, seq_len, 144, 256, 3)))
-    #model.add(keras.Input(batch_shape=(6, seq_len, *image_shape)))
 
-    if do_normalization:
-        model = generate_normalization_layers(model)
+    model.add(my_time_model)
 
-    if do_augmentation:
-        model = generate_augmentation_layers(model, augmentation_params)
-
-    model = generate_convolutional_layers(model)
-    print(model.summary())
     model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
-    model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=0.2)))
-    model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=64,   activation='linear')))
+    model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=1024,   activation='linear')))
+    model.add(keras.layers.TimeDistributed(keras.layers.Dropout(rate=DROPOUT)))
+
+    #print(model.summary())
 
     return model
 
