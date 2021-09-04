bedb5aa9329d2b0ee2678d8281637c644b6fd263
raminmh <rhasani@mit.edu>
2021-09-04 15:59:48 -0400

    typo fix

--- tf_data_loader.py
+++ tf_data_loader.py
@@ -83,7 +83,7 @@ def load_dataset_multi(root, image_size, seq_len, shift, stride, label_scale):
 
         for ix in range(n_images):
             #dataset_np[ix] = imread(os.path.join(root, d, '%06d.jpeg' % ix))
-            img = Image.open(os.path.join(root, dname, '%06d.%s' % (ix, file_ending)))
+            img = Image.open(os.path.join(root, d, '%06d.%s' % (ix, file_ending)))
             dataset_np[ix] = img[img.shape[0] - image_size[0]:, :, :]
 
         images_dataset = tf.data.Dataset.from_tensor_slices(dataset_np)
