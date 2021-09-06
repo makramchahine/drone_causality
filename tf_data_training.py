8013aed770ce726514095ba71689f939c8846a6b
raminmh <rhasani@mit.edu>
2021-09-06 08:38:10 -0400

    -

--- tf_data_training.py
+++ tf_data_training.py
@@ -166,7 +166,7 @@ model.summary(line_length=80)
 time_str = time.strftime("%Y:%m:%d:%H:%M:%S")
 if args.model == 'ctrnn':
     file_path = os.path.join(args.save_dir,
-                             'rev-%d_ct_type-%s_seq-%d_opt-%s_lr-%f_crop-%f_epoch-{epoch:03d}_val_loss:{val_loss:.4f}_loss:{loss:.4f}_%s' % (REV, args.model, args.ct_type, args.seq_len, args.opt, args.lr, args.top_crop, time_str)),
+                             'rev-%d_ct_type-%s_seq-%d_opt-%s_lr-%f_crop-%f_epoch-{epoch:03d}_val_loss:{val_loss:.4f}_loss:{loss:.4f}_%s' % (REV, args.ct_type, args.seq_len, args.opt, args.lr, args.top_crop, time_str)),
 else:
     file_path = os.path.join(args.save_dir,
                              'rev-%d_model-%s_seq-%d_opt-%s_lr-%f_crop-%f_epoch-{epoch:03d}_val_loss:{val_loss:.4f}_loss:{loss:.4f}_%s' % (
